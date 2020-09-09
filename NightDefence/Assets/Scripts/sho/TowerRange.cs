c670ca8663a93b3281ee547ddffd090578ac4743
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-09-09 16:10:39 +0200

    range indecator
    -all towers now have a range indecator
    -colors still need to be applied

--- NightDefence/Assets/Scripts/sho/TowerRange.cs
+++ NightDefence/Assets/Scripts/sho/TowerRange.cs
@@ -20,6 +20,7 @@ public class TowerRange : MonoBehaviour
         }
     }
 
+    //basic
     public void Range_BasicOn()
     {
         rangeIndecators[0].SetActive(true);
@@ -28,5 +29,45 @@ public class TowerRange : MonoBehaviour
     public void Range_BasicOff()
     {
         rangeIndecators[0].SetActive(false);
+    }
+    //sniper
+    public void Range_SniperOn()
+    {
+        rangeIndecators[1].SetActive(true);
+        rangeIndecators[1].transform.position = rangePosition;
+    }
+    public void Range_SniperOff()
+    {
+        rangeIndecators[1].SetActive(false);
+    }
+    //slow
+    public void Range_SlowOn()
+    {
+        rangeIndecators[2].SetActive(true);
+        rangeIndecators[2].transform.position = rangePosition;
+    }
+    public void Range_SlowOff()
+    {
+        rangeIndecators[2].SetActive(false);
+    }
+    //shotgun
+    public void Range_shotgunOn()
+    {
+        rangeIndecators[3].SetActive(true);
+        rangeIndecators[3].transform.position = rangePosition;
+    }
+    public void Range_ShotgunOff()
+    {
+        rangeIndecators[3].SetActive(false);
+    }
+    //laser
+    public void Range_LaserOn()
+    {
+        rangeIndecators[4].SetActive(true);
+        rangeIndecators[4].transform.position = rangePosition;
+    }
+    public void Range_LaserOff()
+    {
+        rangeIndecators[4].SetActive(false);
     }
 }
