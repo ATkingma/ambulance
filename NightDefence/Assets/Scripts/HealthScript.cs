946e7132c6444de195eaa7b42f926113386b2a87
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-09-09 13:53:45 +0200

    range indecators work with placement now
    -added range indecator with placement
    -killing enemies gives centjes

--- NightDefence/Assets/Scripts/HealthScript.cs
+++ NightDefence/Assets/Scripts/HealthScript.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class HealthScript : MonoBehaviour
 {
     public float health;
-    
+    public float centjes;
+    private GameObject gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<Centjes>().gameObject;
+    }
     public void DoDamage(float oof)
     {
         print("OOF");
         health -= oof;
+
+        //dood
+        if(health <= 0)
+        {
+            //ragdol ding
+            gameManager.GetComponent<Centjes>().CentjesErBij(centjes);
+        }
     }
 }
---
+++ NightDefence/Assets/Scripts/sho/TowerRange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerRange : MonoBehaviour
+{
+    private Vector3 rangePosition;
+    public List<GameObject> rangeIndecators;
+
+    private void Start()
+    {
+        List<GameObject> rangeIndecators = new List<GameObject>();
+    }
+    private void Update()
+    {
+        if(Input.GetButtonUp("Fire1"))
+        {
+            rangePosition = FindObjectOfType<CubePlacer>().finalPosition;
+        }
+    }
+
+    public void Range_BasicOn()
+    {
+        rangeIndecators[0].SetActive(true);
+        rangeIndecators[0].transform.position = rangePosition;
+    }
+    public void Range_BasicOff()
+    {
+        rangeIndecators[0].SetActive(false);
+    }
+}
