4890de195f9171ae998e685050f67893afceb05f
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-09 10:29:14 +0200

    script
    - voor animaties

---
+++ NightDefence/Assets/Scripts/BigEnemieScriptExtra.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BigEnemieScriptExtra : MonoBehaviour
+{
+    public Animator anim;
+    void Update()
+    {
+        if (gameObject.GetComponent<HealthScript>().health <= 300)
+        {
+            anim.SetBool("Running", true);
+            gameObject.GetComponent<NavMeshAgent>().speed = 3;
+        }
+    }
+}
