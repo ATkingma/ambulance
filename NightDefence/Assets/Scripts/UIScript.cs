e5be6c689984a5c4657f73c940b92590ffca52fe
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-12 13:21:28 +0200

    more ui
    ui hoeveel enemies je moet dood maken

--- NightDefence/Assets/Scripts/UIScript.cs
+++ NightDefence/Assets/Scripts/UIScript.cs
@@ -12,7 +12,7 @@ public class UIScript : MonoBehaviour
     {
         wave = gameObject.GetComponent<WaveScript>().waveCount;
         money.text = gameObject.GetComponent<Centjes>().centjes.ToString();
-        enemiesAlive = gameObject.GetComponent<WaveScript>().waves[wave].enemies[0].amount + gameObject.GetComponent<WaveScript>().waves[wave].enemies[1].amount + gameObject.GetComponent<WaveScript>().waves[wave].enemies[2].amount;
+        enemiesAlive = gameObject.GetComponent<WaveScript>().maxEnemies - gameObject.GetComponent<WaveScript>().enemiesDied;
         EnemiesAlive.text = enemiesAlive.ToString();
         baseHP.text = basis.GetComponent<HealthScript>().health.ToString();
     }
