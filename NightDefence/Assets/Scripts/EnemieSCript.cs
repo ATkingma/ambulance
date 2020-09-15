ded5fd4ae5a1e207286deeb4a3003438095f7f48
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-09-15 13:18:13 +0200

    time of enemy death
    -changed from 0.5 to 0.1 to fix tower aim

--- NightDefence/Assets/Scripts/EnemieSCript.cs
+++ NightDefence/Assets/Scripts/EnemieSCript.cs
@@ -103,7 +103,7 @@ public class EnemieSCript : MonoBehaviour
         {
             GiveVariableToWaveScript();
         }
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
 
     }
