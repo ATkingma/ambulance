9fdcbaf04209e11322fb58d7c6a55352fc1fd8ad
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-15 17:22:22 +0200

    ui script

--- NightDefence/Assets/Scripts/UIScript.cs
+++ NightDefence/Assets/Scripts/UIScript.cs
@@ -1,19 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
     public Text money, baseHP, EnemiesAlive;
     public int wave, enemiesAlive;
-    public GameObject basis;
+    public GameObject basis,ingameUi,InGameMenu,GameManager;
+    public bool escape;
     public void Update()
     {
-        wave = gameObject.GetComponent<WaveScript>().waveCount;
-        money.text = gameObject.GetComponent<Centjes>().centjes.ToString();
-        enemiesAlive = gameObject.GetComponent<WaveScript>().maxEnemies - gameObject.GetComponent<WaveScript>().enemiesDied;
+        InGameUIUpdate();
+        if (Input.GetButtonDown("Esc"))
+        {
+            ESCMenu();
+            print("koek");
+        }
+    }
+    public void InGameUIUpdate()
+    {
+        wave = GameManager.GetComponent<WaveScript>().waveCount;
+        money.text = GameManager.GetComponent<Centjes>().centjes.ToString();
+        enemiesAlive = GameManager.GetComponent<WaveScript>().maxEnemies - GameManager.GetComponent<WaveScript>().enemiesDied;
         EnemiesAlive.text = enemiesAlive.ToString();
         baseHP.text = basis.GetComponent<HealthScript>().health.ToString();
+    }
+    public void ESCMenu()
+    {
+        if (escape == false)
+        {
+            ingameUi.gameObject.SetActive(false);
+            InGameMenu.gameObject.SetActive(true);
+            escape = true;
+        }
+        if (escape == true)
+        {
+            InGameMenu.gameObject.SetActive(false);
+            ingameUi.gameObject.SetActive(true);
+            escape = false;
+        }
+    }
+    public void Options()
+    {
+      
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+      Application.Quit();
     }
 }
