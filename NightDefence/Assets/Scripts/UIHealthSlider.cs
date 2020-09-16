d27676b1a02b3dbe3acca737086ee55549b3729a
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-16 10:56:26 +0200

    health bar enemies
    een health script en health bar voor alle enemies

---
+++ NightDefence/Assets/Scripts/UIHealthSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthSlider : MonoBehaviour
+{
+    public GameObject mainCamera,enemy;
+    public Slider healthSlider;
+    
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        healthSlider.maxValue = enemy.GetComponent<HealthScript>().health;
+        healthSlider.minValue = 0;
+    }
+    void Update()
+    {
+        transform.LookAt(mainCamera.transform.position);
+        healthSlider.value = enemy.GetComponent<HealthScript>().health;
+    }
+}
