e1a6680d15f9c27d3f3928c75cdcfefa3ecf23bf
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-16 11:27:34 +0200

    main menu en meer in desc
    main menu werkt
    option prefab

---
+++ NightDefence/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject options, credits;
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Options()
+    {
+        UiOff();
+        options.SetActive(true);
+    }
+    public void Credits()
+    {
+        UiOff();
+        credits.SetActive(true);
+    }
+    public void UiOff()
+    {
+        options.SetActive(false);
+        credits.SetActive(false);
+    }
+}
