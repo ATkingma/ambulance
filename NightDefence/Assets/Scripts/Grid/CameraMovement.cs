dc68b5231e8ee7a58f578c3431e9d62baf2de4f4
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-09-24 21:32:31 +0200

    partly starten on sleection

--- NightDefence/Assets/Scripts/Grid/CameraMovement.cs
+++ NightDefence/Assets/Scripts/Grid/CameraMovement.cs
@@ -17,23 +17,7 @@ public class CameraMovement : MonoBehaviour
     {
         moveDir.z = -Input.GetAxisRaw("Horizontal");
         moveDir.x = Input.GetAxisRaw("Vertical");
-        if (Time.timeScale == 1)
-        {
-            controller.Move(-moveDir.normalized * 1);
-        }
-        if (Time.timeScale == 2)
-        {
-            controller.Move(-moveDir.normalized * 0.5f);
-        }
-        if (Time.timeScale == 5)
-        {
-            controller.Move(-moveDir.normalized * 0.2f);
-        }
-        if (Time.timeScale == 10)
-        {
-            controller.Move(-moveDir.normalized * 0.1f);
-        }
-        
+        controller.Move(-moveDir.normalized / Time.timeScale);
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0f) // forward
         {
