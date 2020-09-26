10423b28b1e254cfbc1bfb42619f62e50064a548
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-09-26 19:13:03 +0200

    fulscreen toggle en res script
    alles werkt alleen res doet raar

--- NightDefence/Assets/Scripts/MainMenuScript.cs
+++ NightDefence/Assets/Scripts/MainMenuScript.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject options, credits,mainMenu, controlls,hover;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropDown;
+    public void Start()
+    {
+        resolutions=Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0; i <resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+              currentResolutionIndex=i;
+            }
+        }   
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void QuitGame()
     {
         Application.Quit();
@@ -44,5 +74,9 @@ public class MainMenuScript : MonoBehaviour
     public void PlayerHover()
     {
         hover.GetComponent<AudioSource>().Play();
+    }
+    public void SetFullScreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
