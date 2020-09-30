2fb15bdcedfa6573592753f12c3f55838e121950
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-09-30 11:12:12 +0200

    tower level added ontop of tower

---
+++ NightDefence/Assets/Scripts/Towers/TowerLevelUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TowerLevelUI : MonoBehaviour
+{
+    public GameObject tower;
+    public TextMeshProUGUI text;
+
+    void Update()
+    {
+        if(tower.GetComponent<TurretTargeting>() != null)
+        {
+            text.text = tower.GetComponent<TurretTargeting>().upgradeCount.ToString();
+        }
+        else
+        {
+            text.text = tower.GetComponent<LaserTargeting>().upgradeCount.ToString();
+        }
+    }
+}
