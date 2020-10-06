134e46f16a56f24671bb74d997551553c48f1fb5
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-10-06 02:20:38 +0200

    veel changes
    wat anders sounds
    meer prefabs van enemies gemaakt
    en oude verwijderd
    of iets anders

--- NightDefence/Assets/Scripts/BigEnemieScriptExtra.cs
+++ NightDefence/Assets/Scripts/BigEnemieScriptExtra.cs
@@ -3,10 +3,16 @@ using UnityEngine.AI;
 
 public class BigEnemieScriptExtra : MonoBehaviour
 {
+    private float hp;
     public Animator anim;
+    private void Start()
+    {
+        hp += gameObject.GetComponent<HealthScript>().health;
+        hp /= 2;
+    }
     void Update()
     {
-        if (gameObject.GetComponent<HealthScript>().health <= 300)
+        if (gameObject.GetComponent<HealthScript>().health <= hp)
         {
             anim.SetBool("Running", true);
             gameObject.GetComponent<NavMeshAgent>().speed = 3;
