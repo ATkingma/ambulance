ac0a4371f9b7eaf6c2ffa6183d40552e2c3156d1
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-10-10 12:51:15 +0200

    more ui

---
+++ NightDefence/Assets/Scripts/BaseHealthSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseHealthSlider : MonoBehaviour
+{
+    public GameObject baze;
+    public Slider healthSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider.maxValue = baze.GetComponent<HealthScript>().health;
+        healthSlider.minValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = baze.GetComponent<HealthScript>().health;
+    }
+}
