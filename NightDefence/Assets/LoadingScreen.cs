34eaa2ea43001cb089063c5f5f9ed1da9249477f
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-10-14 16:15:04 +0200

    loading screen en meer updates en ui scaling fix want ik was dom

---
+++ NightDefence/Assets/LoadingScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public float time;
+    public Slider slider;
+    public int scene;
+    void Start()
+    {
+        slider.maxValue = 5000;
+        slider.minValue = 0;
+    }
+    void Update()
+    {
+        if (time >= 5000)
+        {
+            SceneManager.LoadScene(scene);
+        }
+        slider.value = time;
+        Counter();
+    }
+    public void Counter()
+    {
+        time++;
+    }
+}
