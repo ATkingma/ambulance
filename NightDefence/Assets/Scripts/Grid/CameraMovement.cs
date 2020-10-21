1ad5930aabe0acab078ff8979c183e19de8685e4
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-10-21 08:16:47 +0200

    camera movement better!
    -camera now fixed within min height and max height.
    -grid location indecator works fine again!

--- NightDefence/Assets/Scripts/Grid/CameraMovement.cs
+++ NightDefence/Assets/Scripts/Grid/CameraMovement.cs
@@ -7,7 +7,7 @@ public class CameraMovement : MonoBehaviour
     CharacterController controller;
     private Vector3 moveDir;
     public Camera mainCam;
-    public float zoomSpeed;
+    public float zoomSpeed, minHeight, maxHeight;
 
     private void Start()
     {
@@ -21,11 +21,17 @@ public class CameraMovement : MonoBehaviour
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0f) // forward
         {
-            controller.Move(new Vector3(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"), 0) * -zoomSpeed);
+            if(transform.position.y < maxHeight)
+            {
+                controller.Move(new Vector3(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"), 0) * -zoomSpeed);
+            }
         }
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
         {
-            controller.Move(new Vector3(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"), 0) * -zoomSpeed);
+            if (transform.position.y > minHeight)
+            {
+                controller.Move(new Vector3(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"), 0) * -zoomSpeed);
+            }
         }
     }
 }
