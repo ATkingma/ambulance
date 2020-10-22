7d04848057ec5c20990269002cdb5f583d74be8e
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-10-22 20:30:12 +0200

    level 2 rotate building things
    they now sometimes top rotating for half a sec.

--- NightDefence/Assets/Scripts/Rotate.cs
+++ NightDefence/Assets/Scripts/Rotate.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rotate : MonoBehaviour
 {
     public Vector3 speed;
+    private float stutter;
+
+    private void Start()
+    {
+        stutter = speed.y;
+        Stutter();
+    }
     void Update()
     {
         transform.Rotate(speed * Time.deltaTime);
         
+        if(SceneManager.GetActiveScene().name == "LVL2")
+        {
+            transform.Rotate(speed * Time.deltaTime);
+        }
+    }
+    public void Stutter()
+    {
+        speed.y = 0;
+        Invoke("Stutter2", 0.5f);
+    }
+    public void Stutter2()
+    {
+        speed.y = stutter;
+        Invoke("Stutter", Random.Range(1, 10));
     }
 }
